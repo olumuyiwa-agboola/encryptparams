6375cdb4b905f711a8c4832a7d3f6d343d5ae5b6
olumuyiwa-agboola <agboolaod@gmail.com>
2025-01-02 12:27:01 +0100

    Project setup

---
+++ src/EncryptParams.Web/Program.cs
@@ -0,0 +1,23 @@
+using EncryptParams.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>();
+
+app.Run();
